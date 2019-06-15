ce022900e7371275733c4bbdc173b00f67974326
Bethany <blmeier@gmail.com>
2019-06-15 14:20:32 -0400

    Add JS for gameplay

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 const userScore_span = document.getElementById('user-score');
 const computerScore_span = document.getElementById('computer-score');
 const scoreBoard_div = document.querySelector('.score-board');
@@ -7,3 +7,58 @@ const result_div = document.querySelector('.result');
 const rock_div = document.getElementById('rock');
 const paper_div = document.getElementById('paper');
 const scissors_div = document.getElementById('scissors');
+
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    return computerChoice;
+}
+
+function win() {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+}
+
+function lose() {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+}
+
+function draw() {
+    console.log('draw');
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case 'rockscissors':
+        case 'paperrock':
+        case 'scissorspaper':
+            win();
+            break;
+        case 'rockpaper':
+        case 'paperscissors':
+        case 'scissorsrock':
+            lose();
+            break;
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorsscissors':
+            draw();
+            break;
+    }
+}
+
+function main() {
+    rock_div.addEventListener('click', function() {
+        game('rock');
+    })
+    paper_div.addEventListener('click', function() {
+        game('paper');
+    })
+    scissors_div.addEventListener('click', function() {
+        game('scissors');
+    })
+}
+
+main();
